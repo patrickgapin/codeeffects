ac597e26d6f693b228fd54946e962a452a22632a
Patrick GA <patrickgapin@gmail.com>
2016-06-30 16:08:24 -0400

    wip

--- codeeffects-sample01/Rules/DepositLimits/DepositLimitHelper.cs
+++ codeeffects-sample01/Rules/DepositLimits/DepositLimitHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using codeeffects_sample01.Models;
+using codeeffects_sample01.Models.CreditCard;
 using CodeEffects.Rule.Attributes;
 
 namespace codeeffects_sample01.Rules.DepositLimits
@@ -11,5 +12,23 @@ namespace codeeffects_sample01.Rules.DepositLimits
     {
         public string Test { get; set; }
 
+        // The Parameter passed to an external Action method can be: Value type, String, Enumerable or Source object type.
+        [Action(DisplayName = "-- Accept Card --")]
+        public void Validate(DepositLimitRule rule)
+        {
+            rule.CreditCard.EvaluationResult = rule.CreditCard.IsCreditCardAmountValid() ? Result.Pass : Result.Fail;
+        }
+
+        // This will be an External Method referenced from our ObjectSource element.
+        [Method(DisplayName = " -- Card Amount Is Valid --")]
+        public bool IsCreditCardAmountValid(DepositLimitRule rule)
+        {
+            var cardsLimitsHelper = new Limits();
+
+            var isMinValid = rule.CreditCard.Amount >= cardsLimitsHelper.CardLimits[rule.CreditCard.Type].Item1;
+            var isMaxValid = rule.CreditCard.Amount <= cardsLimitsHelper.CardLimits[rule.CreditCard.Type].Item2;
+
+            return isMinValid && isMaxValid;
+        }
     }
 }
--- codeeffects-sample01/Rules/DepositLimits/DepositLimitRule.cs
+++ codeeffects-sample01/Rules/DepositLimits/DepositLimitRule.cs
@@ -3,40 +3,24 @@ using codeeffects_sample01.Models;
 using codeeffects_sample01.Models.CreditCard;
 using CodeEffects.Rule.Attributes;
 
+// Dynamic Menu Data Sources
+// http://www.codeeffects.com/Doc/Business-Rules-Dynamic-Menu-Data-Sources
+// External Method Attribute
+
+// http://codeeffects.com/Doc/Business-Rule-External-Method-Attribute
 namespace codeeffects_sample01.Rules.DepositLimits
 {
-    // Dynamic Menu Data Sources
-    // http://www.codeeffects.com/Doc/Business-Rules-Dynamic-Menu-Data-Sources
-    // External Method Attribute
-    // http://codeeffects.com/Doc/Business-Rule-External-Method-Attribute
-
-    //[ExternalMethod(typeof(CreditCard), "IsAmountValid")]
-
+    [Source(DeclaredMembersOnly = true)] // Prevents loading members from another partial class whichis
+    [ExternalAction(typeof(DepositLimitHelper), "Validate")]
+    [ExternalMethod(typeof(DepositLimitHelper), "IsCreditCardAmountValid")]
     public class DepositLimitRule
     {
         public CreditCard CreditCard { get; set; }
 
         [ExcludeFromEvaluation]
         public Result EvaluationResult;
-
-        [Method(DisplayName = " -- Has a valid amount --")]
-        public bool IsCreditCardAmountValid()
-        {
-            var cardsLimitsHelper = new Limits();
-
-            var isMinValid = CreditCard.Amount >= cardsLimitsHelper.CardLimits[CreditCard.Type].Item1;
-            var isMaxValid = CreditCard.Amount <= cardsLimitsHelper.CardLimits[CreditCard.Type].Item2;
-
-            return isMinValid && isMaxValid;
-        }
-
-        [Action(DisplayName = "-- Accept Card --")]
-        public void Validate()
-        {
-            this.EvaluationResult = IsCreditCardAmountValid() ? Result.Pass : Result.Fail;
-        }
-        
-
+       
+        // Use of in-rule method.
         [Method("-- Card Amount is More than 2000 --")]
         public bool CardAmountHigherThan2000() { return CreditCard.Amount > 2000; }
     }
